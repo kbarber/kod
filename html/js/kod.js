7d289f6fc9ea17abaa6172b6a242bcab84e53bf4
Ken Barber <ken@bob.sh>
2012-11-30 12:22:33 +1100

    Get basic websockets implementation working

--- html/js/kod.js
+++ html/js/kod.js
@@ -1,47 +1,114 @@
-var world = document.getElementById("world");
+/**
+ * An object that represents the visible world.
+ *
+ * @param {String} canvasId
+ */
+function WorldView(canvasId) {
+  this.world = document.getElementById(canvasId);
+  this.world.width = 400;
+  this.world.height = 400; 
+  this.world.style.width  = '400px';
+  this.world.style.height = '400px';
+  this.ctx = world.getContext("2d");
 
-world.width  = 400;
-world.height = 400; 
-world.style.width  = '400px';
-world.style.height = '400px';
-
-var ctx = world.getContext("2d");
-
-var grass = new Image;
-grass.onload = function() {
-  for (x = 0; x < 12; x++) {
-    for (y = 0; y < 12; y++) {
-      ctx.drawImage(grass, x*32, y*32);
-    }
+  /**
+   * Draw an image onto the main world area as a tile.
+   *
+   * @public
+   * @example
+   * var image = new Image;
+   * grass.src = "img/grass.png";
+   * drawTile(grass, 4, 1);
+   * @param {Image} obj Image object to draw
+   * @param {Number} x X coordinate
+   * @param {Number} y Y coordinate
+   */
+  this.drawTile = function(obj, x, y) {
+    this.ctx.drawImage(obj, x*32, y*32);
   }
-};
+}
+var wv = new WorldView("world");
+
+/**
+ * KodServer connection object.
+ *
+ * @param {String} url WebSocket URL for KodServer
+ */
+function KodServer(url) {
+  var self = this;
+  this.url = url
+  this.ws = new WebSocket(this.url);
+  this.ws.onopen = function() {
+    console.log("socket open: " + url);
+    self.login();
+  }
+  this.ws.onmessage = function(msg) {
+    rcv = JSON.parse(msg.data);
+    console.log("On message: " + rcv);
+  }
+
+  this.login = function() {
+    cmd = {
+      "command":"login",
+      "version":1,
+      "payload":{
+        "username":"ken",
+        "password":"ken"
+      }
+    };
+    snd = JSON.stringify(cmd);
+    this.ws.send(snd);
+  }
+
+  this.close = function() {
+    this.ws.close();
+  }
+}
+
+
+var ks = new KodServer("ws://localhost:7778/");
+
+function loadedImage(str) {
+  console.log("Loaded image: " + str);
+}
+
+/* Prepare images */
+var grass = new Image;
+grass.onload = loadedImage("grass");
 grass.src = "img/grass.png";
 
 var knight = new Image;
-knight.onload = function() {
-  ctx.drawImage(knight, 0, 0);
-};
+grass.onload = loadedImage("knight");
 knight.src = "img/knight.png";
 
 var cthulhu = new Image;
-cthulhu.onload = function() {
-  ctx.drawImage(cthulhu, 32, 0);
-};
+cthulhu.onload = loadedImage("cthulhu");
 cthulhu.src = "img/cthulhu.png";
 
 var rabbit = new Image;
-rabbit.onload = function() {
-  ctx.drawImage(rabbit, 96, 0);
-};
+rabbit.onload = loadedImage("rabbit");
 rabbit.src = "img/rabbit.png";
 
 var cs = new Image;
-cs.onload = function() {
+cs.onload = loadedImage("cobblestone");
+cs.src = "img/cobblestone.png";
+
+setTimeout(function() {
+  for (x = 0; x < 12; x++) {
+    for (y = 0; y < 12; y++) {
+      wv.drawTile(grass, x, y);
+    }
+  }
+
+  wv.drawTile(knight, 0, 0);
+  wv.drawTile(cthulhu, 1, 0);
+  wv.drawTile(rabbit, 3, 0);
+
   for (y = 0; y < 12; y++) {
-    ctx.drawImage(cs, 5*32, y*32);
+    wv.drawTile(cs, 5, y);
   }
   for (x = 5; x < 12; x++) {
-    ctx.drawImage(cs, x*32, 5*32);
+    wv.drawTile(cs, x, 5);
   }
-};
-cs.src = "img/cobblestone.png";
+}, 3000)
+
--- server.js
+++ server.js
@@ -3,3 +3,15 @@ var connect = require('connect');
 connect.createServer(
     connect.static("html")
 ).listen(7777);
+
+var WebSocketServer = require('ws').Server
+  , wss = new WebSocketServer({port: 7778});
+wss.on('connection', function(ws) {
+    ws.on('message', function(message) {
+        rcv = JSON.parse(message);
+        snd = JSON.stringify({"bar":"far"});
+        console.log('received: %s', rcv);
+        ws.send(snd);
+    });
+    ws.send(JSON.stringify('welcome!'));
+});
